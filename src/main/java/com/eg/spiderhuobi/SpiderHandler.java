b10978cbfa94efed196276641aa579b6d198cfdc
makewheels <makewheels>
2021-11-28 00:17:14 +0800

    commit

--- src/main/java/com/eg/spiderhuobi/SpiderHandler.java
+++ src/main/java/com/eg/spiderhuobi/SpiderHandler.java
@@ -1,18 +1,56 @@
 package com.eg.spiderhuobi;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.github.makewheels.s3util.S3Config;
+import com.github.makewheels.s3util.S3Service;
+
+import java.io.File;
+import java.time.LocalDateTime;
 
 public class SpiderHandler {
+    private final S3Service s3Service = new S3Service();
+
     public void initS3() {
-
+        S3Config s3Config = new S3Config();
+        s3Config.setEndpoint(System.getenv("s3_endpoint"));
+        s3Config.setRegion(System.getenv("s3_region"));
+        s3Config.setBucketName(System.getenv("s3_bucketName"));
+        s3Config.setAccessKey(System.getenv("s3_accessKey"));
+        s3Config.setSecretKey(System.getenv("s3_secretKey"));
+        s3Service.init(s3Config);
     }
 
-    public JSONObject getConfig() {
-        return null;
+    private JSONObject getConfig() {
+        File configFile = new File(SpiderHandler.class.getResource("/config.json").getPath());
+        String configJson = FileUtil.readUtf8String(configFile);
+        return JSONObject.parseObject(configJson);
     }
 
-    public void doCrawl() {
+    public void crawl() {
+        JSONObject config = getConfig();
+        JSONArray requests = config.getJSONArray("requests");
+        for (int i = 0; i < requests.size(); i++) {
+            JSONObject request = requests.getJSONObject(i);
+            String requestName = request.getString("requestName");
+            JSONObject source = request.getJSONObject("source");
+            String url = source.getString("url");
+            String response = HttpUtil.get(url);
+            save(config, requestName, response);
+        }
+    }
 
+    private void save(JSONObject config, String requestName, String response) {
+        JSONObject target = config.getJSONObject("target");
+        String missionName = config.getString("missionName");
+        String path = target.getString("path");
+        path = path.replace("${missionName}", missionName);
+        path = path.replace("${requestName}", requestName);
+        path = path.replace("${GMTString}", LocalDateTime.now().toString());
+        s3Service.putObject(path, response);
     }
 
 }
--- src/main/java/com/eg/spiderhuobi/StartAliyunFC.java
+++ src/main/java/com/eg/spiderhuobi/StartAliyunFC.java
@@ -2,15 +2,18 @@ package com.eg.spiderhuobi;
 
 import com.aliyun.fc.runtime.Context;
 import com.aliyun.fc.runtime.StreamRequestHandler;
+import com.github.makewheels.s3util.S3Service;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StartAliyunFC implements StreamRequestHandler {
+    private final SpiderHandler spiderHandler = new SpiderHandler();
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) {
-        System.out.println(System.currentTimeMillis());
+        spiderHandler.initS3();
+        spiderHandler.crawl();
     }
 }
